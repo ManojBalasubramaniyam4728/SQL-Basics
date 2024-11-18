2e492295ab10bbd59af811fdc7a50147555fb1b1
ManojBalasubramaniyam4728 <manojbalasubramaniyam4488@gmail.com>
2024-11-18 11:08:46 +0530

    add on querys

--- Query10.sql
+++ Query10.sql
@@ -30,6 +30,53 @@ select * from constab2;
 create table constab3(name varchar(20), id int(20) check(int)>10);
 insert into constab3(name,id) values('gggggg',1);#error
 
+#"Primary Key"
+# The Key which is unique and not null is know as primary key 
+#In A table there can be only one primary key but two or more primary column.
+Create table persons(id int (50), name varchar(20), primary key(id));
+
+#"Foregin key"
+#The Key which refers to primary key in other table.
+create table customersDetails(CustomerID int(50), 
+address varchar(20), zipcode varchar(50),
+ primary key(CustomerID));
+ 
+create table orderDetails(OrderID int(50),
+ CustomerID int(20), Amount int(50),date varchar(50),
+ primary key(OrderID), foreign key(CustomerID)
+ references customersdetails(customerId));
+ 
+insert into customersdetails values(123 ,'ccccc','dfghj123');
+insert into orderDetails values(123 ,123,4500, 'july');
+insert into orderDetails values(123 ,1235,4500, 'july'); #error
+
+select * from customersdetails;
+select * from orderDetails;
+
+delete from customersdetails where CustomerID=123; #error
+
+#On delete cascade
+create table customersDetails2(CustomerID int(50), 
+address varchar(20), zipcode varchar(50),
+ primary key(CustomerID));
+ 
+create table orderDetails2(OrderID int(50),
+ CustomerID int(20), Amount int(50),date varchar(50),
+ primary key(OrderID), foreign key(CustomerID)
+ references customersdetails2(customerId) on delete cascade);
+ 
+insert into customersdetails2 values(123 ,'ccccc','dfghj123');
+insert into orderDetails2 values(123 ,123,4500, 'july');
+
+delete from customersdetails2 where customerId="123";
+
+select * from customersdetails2; # null result
+select * from orderDetails2; # null result
+
+
+
+ 
+
 
 
 
